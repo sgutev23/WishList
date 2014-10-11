d03f2d2bf7ee671546405faec9b6f9c83b0721ac
Cotizo Sima <simacotizo@gmail.com>
2014-10-11 10:17:20 +0200

    example method to get hash for fb
    https://developers.facebook.com/docs/android/getting-started

--- app/src/main/java/com/hackzurich/wishlist/MainActivity.java
+++ app/src/main/java/com/hackzurich/wishlist/MainActivity.java
@@ -5,12 +5,19 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 
@@ -43,6 +50,23 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+    void getHashForFB () {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.hackzurich.wishlist",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 
 
