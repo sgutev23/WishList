ac2ae6df433ef21df2f27d7dd62a3191f20e82c0
Cotizo Sima <simacotizo@gmail.com>
2014-10-11 21:45:47 +0200

    fixing wish ID

--- app/src/main/java/com/hackzurich/wishlist/model/Wish.java
+++ app/src/main/java/com/hackzurich/wishlist/model/Wish.java
@@ -4,18 +4,18 @@ package com.hackzurich.wishlist.model;
  * Created by cotizo on 10/11/2014.
  */
 public class Wish {
-    private final Integer id;
+    private final String id;
     private final String content;
     private final String bought;
 
-    public Wish(Integer id, String content, String bought) {
+    public Wish(String id, String content, String bought) {
         this.id = id;
         this.content = content;
         this.bought = bought;
     }
 
     public Wish(String content) {
-        this(-1, content, null);
+        this(null, content, null);
     }
 
     public String getBought() {
@@ -26,7 +26,7 @@ public class Wish {
         return content;
     }
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
